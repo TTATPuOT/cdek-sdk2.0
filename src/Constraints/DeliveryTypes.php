b8b397892e1259ebbb6764e2ecb5379049a04bdf
vladislavtkacenko <vladisla.tkacenko@x5.ru>
2025-05-10 11:21:02 +0300

    Константы режимов доставки

---
+++ src/Constraints/DeliveryTypes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CdekSDK2\Constraints;
+
+class DeliveryTypes
+{
+    public const DOOR_DOOR = 1;
+    public const DOOR_WAREHOUSE = 2;
+    public const WAREHOUSE_DOOR = 3;
+    public const WAREHOUSE_WAREHOUSE = 4;
+    public const TERMINAL_TERMINAL = 5;
+    public const DOOR_POSTAMATE = 6;
+    public const WAREHOUSE_POSTAMATE = 7;
+    public const POSTAMATE_DOOR = 8;
+    public const POSTAMATE_WAREHOUSE = 9;
+    public const POSTAMATE_POSTAMATE = 10;
+}
